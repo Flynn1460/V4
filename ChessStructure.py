24d7f2273f975924f72b0f297365734098bdfbe5
Flynn Graham <flynngraham101@gmail.com>
2023-03-12 19:16:41 +0000

    GUI Format setup

--- ChessStructure.py
+++ ChessStructure.py
@@ -1,13 +1,21 @@
 import chess as ch
+import pygame as pg
+import time
 
 import Engine as en
 import otherFunctions as of
 import playFunctions as pf
 
+SQ_COLOR_WHITE = (237, 214, 176)
+SQ_COLOR_BLACK = (184, 135, 98)
+
 
 def playerPlay(board):
-    colour = of.opt("Which colour do you want to play?", "w", "b")
-    depth = of.opt("What depth should the computer search to? (rec 4)", "int")
+    global noPrint, depth
+
+    if not noPrint:
+        colour = of.opt("Which colour do you want to play?", "w", "b")
+        depth = of.opt("What depth should the computer search to? (rec 4)", "int")
 
     if colour == "w":
         board = playerMove(board)
@@ -15,28 +23,34 @@ def playerPlay(board):
     while True:
         board = engineMove(board, colour, depth)
         if pf.isGameEnd(board):
-            return
-        print("\n", board)
+            return pf.isGameEnd(board)
+        
+        pf.load(dis, board, SQ_COLOR_WHITE, SQ_COLOR_BLACK)
 
         board = playerMove(board)
         if pf.isGameEnd(board):
-            return
-        print("\n", board)
+            return pf.isGameEnd(board)
+        
+        pf.load(dis, board, SQ_COLOR_WHITE, SQ_COLOR_BLACK)
 
 def computerPlay(board):
-    
-    depth = of.opt("What depth should the computer search to? (rec 4)", "int")
+    global noPrint, depth
+
+    if not noPrint:
+        depth = of.opt("What depth should the computer search to? (rec 4)", "int")
 
     while True:
         board = engineMove(board, "w", depth)
         if pf.isGameEnd(board):
-            return
-        print("\n", board)
+            return pf.isGameEnd(board)
+        
+        pf.load(dis, board, SQ_COLOR_WHITE, SQ_COLOR_BLACK)
 
         board = engineMove(board, "b", depth)
         if pf.isGameEnd(board):
-            return
-        print("\n", board)
+            return pf.isGameEnd(board)
+        
+        pf.load(dis, board, SQ_COLOR_WHITE, SQ_COLOR_BLACK)
 
 
 def playerMove(board):
@@ -55,19 +69,52 @@ def engineMove(board, colour, depth):
     return en.giveBestMove(board)
 
 
-board = ch.Board()
+dis = pg.display.set_mode((800, 800))
+
+pf.loadImages(100)
+
+gamemode = of.opt("What gamemode do you want to play?", "player", "computer")
+autoplay = bool(of.opt("Do you want the games to autoplay?", "yes", "no"))
+
+noPrint = False
+whiteScore, blackScore = 0, 0
+results = [0, 0, 0]
 
 while True:
-    gamemode = of.opt("What gamemode do you want to play?", "player", "computer")
+    board = ch.Board()
 
-    if gamemode == "player":
-        playerPlay(board)
-    elif gamemode == "computer":
-        computerPlay(board)
+    pf.load(dis, board, SQ_COLOR_WHITE, SQ_COLOR_BLACK)
 
-    playAgain = of.opt("Play again?", "yes", "no")
+    while True:
+        if gamemode == "player":
+            val = playerPlay(board)
 
-    if playAgain == "yes":
-        continue
-    elif playAgain == "no":
-        quit()
+        elif gamemode == "computer":
+            val = computerPlay(board)
+
+        
+        break
+
+    noPrint = autoplay
+
+    outcomeInfo = board.outcome()
+    resultCause = str(outcomeInfo.termination)
+
+    if outcomeInfo.result()[0:3] == "1/2":
+        whiteScore += 0.5
+        blackScore += 0.5
+        results[0] += 1
+
+    elif outcomeInfo.result()[0] == "1":
+        whiteScore += 1
+        results[1] += 1
+    
+    elif outcomeInfo.result()[2] == "1":
+        blackScore += 1
+        results[2] += 1
+
+
+    print(str(whiteScore) + " - " + str(blackScore) + "  |  " + "Draws : "  + str(results[0]) + ", " + "White Wins : " + str(results[1]) + ", " + "Black Wins : " + str(results[2]))
+    print("\n")
+
+    continue
--- playFunctions.py
+++ playFunctions.py
@@ -1,11 +1,78 @@
 import chess as ch
+import pygame as pg
+import otherFunctions as of
 
 def isGameEnd(board):
     if board.is_checkmate():
-        return True
+        return True, "Mate"
     
     elif board.is_fivefold_repetition():
-        return True
+        return True, "Mate"
+    
+    elif board.is_stalemate():
+        return True, "Stalemate"
+    
+    elif board.is_insufficient_material():
+        return True, "Insufficient Material"
+    
     
     else:
         return False
+
+
+
+def drawBoard(dis, SQ_COLOR_WHITE, SQ_COLOR_BLACK,  SQUARE_SIZE):
+
+    # Board Color (0 = White, 1 = Black)
+    CURRENT_VAL = 1
+
+    # Colors for computer to refer to
+    SQUARE_COLOR_LIST = [SQ_COLOR_WHITE, SQ_COLOR_BLACK]
+
+    # The color of the board square
+    CURRENT_COLOR = SQUARE_COLOR_LIST[CURRENT_VAL]
+
+    for boardWidth in range(8):
+
+        # Set the starting row color
+        CURRENT_VAL = CURRENT_VAL^1
+        CURRENT_COLOR = SQUARE_COLOR_LIST[CURRENT_VAL]
+
+
+        for boardHeight in range(8):
+
+            # Draw the square
+            pg.draw.rect(dis, CURRENT_COLOR, (SQUARE_SIZE*boardWidth, SQUARE_SIZE*boardHeight, SQUARE_SIZE, SQUARE_SIZE))
+
+            # Flip the square color
+            CURRENT_VAL = CURRENT_VAL^1
+            CURRENT_COLOR = SQUARE_COLOR_LIST[CURRENT_VAL]
+
+def loadImages(SQUARE_SIZE):
+    global imagesDict
+    imagesDict = {}
+
+    for pColor in ["w", "b"]:
+        for pType in ["P", "N", "B", "R", "Q", "K"]:
+
+            if pColor == "w":
+                imagesDict[pType] = pg.transform.scale(pg.image.load("Images/"+pColor+pType+".png"), (SQUARE_SIZE, SQUARE_SIZE))
+            elif pColor == "b":
+                imagesDict[pType.lower()] = pg.transform.scale(pg.image.load("Images/"+pColor+pType+".png"), (SQUARE_SIZE, SQUARE_SIZE))
+
+def drawImages(dis, board):
+    board = of.make_matrix(board)
+
+    for x in range(8):
+        for y in range(8):
+            piece = board[x][y]
+            try:
+                dis.blit(imagesDict[piece], pg.Rect(y * 100, x * 100, 100, 100))
+            
+            except:
+                pass
+
+def load(dis, board, color_white, color_black):
+    drawBoard(dis, color_white, color_black, 100)
+    drawImages(dis, board)
+    pg.display.update()
